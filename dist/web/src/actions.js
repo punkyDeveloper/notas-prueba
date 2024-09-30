4a7047100dd48b0e0aef2874200a766c2a8aa8ef
santiago <elsantihg@gmail.com>
2024-09-29 19:05:40 -0500

    .

--- dist/web/src/actions.js
+++
@@ -1,7 +0,0 @@
-// Define las acciones para cambiar de vista
-export const CHANGE_VIEW = 'CHANGE_VIEW';
-// Acción para cambiar la vista
-export const changeView = (view) => ({
-    type: CHANGE_VIEW,
-    payload: view,
-});
